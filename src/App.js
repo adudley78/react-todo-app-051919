b09c71e4473269b2a39ad34cc95fda6c93b31637
adudley78 <dudleyadam@gmail.com>
2019-05-19 19:42:50 -0400

    Create and mount AddTodo.js and add container and btn class styles to App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 import './App.css';
 
@@ -41,11 +42,13 @@ delTodo = (id) => {
 }
 
 render() {
-    // console.log(this.state.todos)
     return (
       <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
