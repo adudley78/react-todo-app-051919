bb0738aafc16c56389538acb52e67ec410852b0c
adudley78 <dudleyadam@gmail.com>
2019-05-19 17:51:27 -0400

    Rename Todoitem.js to TodoItem.js and add TodoItem component to Todos.js

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class Todoitem extends Component {
+export class TodoItem extends Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ export class Todoitem extends Component {
   }
 }
 
-export default Todoitem
+export default TodoItem
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-
+import TodoItem from './TodoItem';
 
 class Todos extends Component {
     render() {
-        // console.log(this.props.todos)
-    return this.props.todos.map((todo) => (
-        <h3>{ todo.title }</h3>
-    ));
-  }
+        return this.props.todos.map((todo) => (
+            <TodoItem />
+        ));
+    }
 }
 
 export default Todos;
